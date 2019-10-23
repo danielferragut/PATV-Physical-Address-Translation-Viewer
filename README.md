b9fa57e67c25da130834504a9535b65c6b310adb
Daniel Ferragut <danielpferragut@gmail.com>
2019-10-22 23:12:58 -0300

    Can almost print entry, last bytes wrong

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PATV-Physical-Address-Translation-Viewer
-2nd project for the 'Operating Systems' course. Given a process id(pid) returns the physical memory address of the its virtual memory pages
+2nd project for the 'Operating Systems' course. Given a process id(pid) returns the physical memory address of its virtual memory pages.
---
+++ patv.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import struct
+
+
+# TODO: ERRO MUITO PROVAVELMENTE EM COMO PYTHON 3 faz f.read
+# Entry sao os 64 bits do pagemap
+# Diferencas entre o nosso e esperado: Os bytes finais
+# Nosso:    0x8180000000000000
+# Esperado: 0x818000000021367f
+# Acho que é problema do read mesmo, talvez depende do sistema
+def read_entry(path, offset, size=8):
+    with open(path, 'rb') as f:
+        f.seek(offset, 0)
+        return struct.unpack('Q', f.read(size))[0]
+
+
+def main():
+    # Variaveis iniciais
+    argv = sys.argv
+    print(argv)
+    pid = argv[1]
+    proc_pid_path = os.path.join('/proc', pid)
+    maps_path = os.path.join(proc_pid_path, 'maps')
+    pagemap_path = os.path.join(proc_pid_path, 'pagemap')
+    valid_ranges = []
+    maps_line_list = []
+    entry_list = []
+    try:
+        maps_line_list = [line.rstrip('\n') for line in open(maps_path)]
+    except:
+        print("Process is not listed in /proc")
+
+    for i, line in enumerate(maps_line_list):
+        maps_line_list[i] = line.split()
+        # if line[4] != '0':
+        mem_range = maps_line_list[i][0]
+        start, end = mem_range.split(sep='-')
+        start = start[:-3]
+        end = end[:-3]
+        # print(start, end)
+        valid_ranges.append((start, end))
+
+    i = 0
+    for virtual_range in valid_ranges:
+        start = int(virtual_range[0], 16)
+        end = int(virtual_range[1], 16)
+        current = start
+        while current != end:
+            # Offset seria vaddr/page_size * page map entry
+            # Mas já tirei os 3 ultimos digitos (dividir por 4096) e multipliquei por 8 (entrada no pagemaps tem 8 bytes)
+            entry = read_entry(pagemap_path, current*8)
+            pfn = get_pfn(entry)
+            print("Vaddr =  {} Offset = {} Entry: {}".format(hex(current*4096), hex(current*8), hex(entry)))
+            # print("Is Present? : {}".format(is_present(entry)))
+            # print("Is file-page: {}".format(is_file_page(entry)))
+            # print("Page count: {}".format(get_pagecount(pfn)))
+            # print("Page flags: {}".format(hex(get_page_flags(pfn))))
+            current += 1
+
+
+def get_pfn(entry):
+    return entry & 0x7FFFFFFFFFFFFF
+
+
+if __name__ == '__main__':
+    main()
